7c5d1e548f386ef154b98375e6a50a62be15b09c
mandasureshbabu069 <136985240+mandasureshbabu069@users.noreply.github.com>
2023-11-23 01:04:11 +0530

    pom scm modify

--- src/main/java/com/companyname/bank/login.java
+++ src/main/java/com/companyname/bank/login.java
@@ -8,6 +8,6 @@ public class login
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World..modify" );
+        System.out.println( "pom scm modify" );
     }
 }
